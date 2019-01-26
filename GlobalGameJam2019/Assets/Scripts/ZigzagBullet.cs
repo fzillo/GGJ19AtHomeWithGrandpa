be46ec8a0ab9adea34901fc4056c7efaf8d3bbe1
fzillo <fabian.zillgens@gmail.com>
2019-01-26 17:31:37 +0100

    fixes

--- GlobalGameJam2019/Assets/Scripts/ZigzagBullet.cs
+++ GlobalGameJam2019/Assets/Scripts/ZigzagBullet.cs
@@ -7,6 +7,9 @@ public class ZigzagBullet : Bullet
 
     public float timeTillFlip = 3;
     [SerializeField] private float localTime;
+    [SerializeField] private int upDown = 1;
+
+
 
     // Update is called once per frame
     void Update()
@@ -16,10 +19,11 @@ public class ZigzagBullet : Bullet
         if (localTime > timeTillFlip)
         {
             localTime = 0;
-            tf.Rotate(180, 0, 0);
+            upDown *= -1;
+            //tf.Rotate(180, 0, 0);
         }
 
-        Vector3 moveVect = new Vector3(speed * Time.deltaTime, speed * Time.deltaTime, 0);
+        Vector3 moveVect = new Vector3(speed * Time.deltaTime, upDown * speed * Time.deltaTime, 0);
         tf.Translate(moveVect, Space.Self);
     }
 }
