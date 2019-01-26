9b2f6f8596de7790c00fa02ad08d2e4612415483
fzillo <fabian.zillgens@gmail.com>
2019-01-26 14:30:45 +0100

    new Object Wave

--- GlobalGameJam2019/Assets/Scripts/BulletSpawnController.cs
+++ GlobalGameJam2019/Assets/Scripts/BulletSpawnController.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class BulletSpawnController : MonoBehaviour
 {
-    public Transform bulletPrefab;
-    public Transform spawnPosition;
+    public Transform straightBulletPrefab;
+    public Transform spinBulletPrefab;
+    public List<Transform> spawnerList;
 
     void Update()
     {
         if (Input.GetKeyDown("space"))
         {
-            StartCoroutine(SpawnWave(3, spawnPosition, (float)0.3));
+            //StartCoroutine(SpawnWave(3, spinBulletPrefab, spawnerList[0], (float)0.3));
+            Wave w = new Wave(straightBulletPrefab, 1, 1, new int[] { 1 }, 0);
+            StartCoroutine(SpawnWave(w));
         }
     }
 
-    IEnumerator SpawnWave(int nBullets, Transform spawnPosition, float timeBetweenBullets)
+    [System.Obsolete("This is an obsolete method")]
+    IEnumerator SpawnWave(int nBullets, Transform bulletPrefab, Transform spawnPosition, float timeBetweenBullets)
     {
         for (int i = 0; i < nBullets; i++)
         {
             Instantiate(bulletPrefab, spawnPosition.position, spawnPosition.rotation);
             yield return new WaitForSeconds(timeBetweenBullets);
         }
+    }
 
+    IEnumerator SpawnWave(Wave w)
+    {
+        if (w.bulletPrefab == null)
+            Debug.LogError("No Prefab found at SpawnWave!");
+
+        for (int nBullet = 0; nBullet < w.numberBulletsPerSpawn; nBullet++)
+        {
+            for (int i = 0; i < w.spawnindexArray.Length; i++)
+            {
+                Instantiate(w.bulletPrefab, spawnerList[i].position, spawnerList[i].rotation);
+                yield return new WaitForSeconds(w.delayBetweenSpawns);
+            }
+        }
     }
 
 }
---
+++ GlobalGameJam2019/Assets/Scripts/Wave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public Transform bulletPrefab;
+    public int numberBulletsPerSpawn = 1;
+    public float timeBetweenBullets = 1;
+    public int[] spawnindexArray;
+    public float delayBetweenSpawns = 0;
+
+    public Wave(Transform bulletPrefab, int numberBulletsPerSpawn, float timeBetweenBullets, int[] spawnindexArray, float delayBetweenSpawns)
+    {
+        this.bulletPrefab = bulletPrefab;
+        this.numberBulletsPerSpawn = numberBulletsPerSpawn;
+        this.timeBetweenBullets = timeBetweenBullets;
+        this.spawnindexArray = spawnindexArray;
+        this.delayBetweenSpawns = delayBetweenSpawns;
+    }
+}
