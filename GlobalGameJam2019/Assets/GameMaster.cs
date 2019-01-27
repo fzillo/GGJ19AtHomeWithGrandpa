3acf526dd89b8139d4c72ffbf6da8630429f9db2
felix baral <felixbaral@me.com>
2019-01-27 09:18:52 +0100

    gameMaster

---
+++ GlobalGameJam2019/Assets/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public BulletSpawnController bulletSpawnController;
+    
+    public float bulletIntervalSeconds = 2f;
+    public float lastSpawn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time-lastSpawn > bulletIntervalSeconds)
+        {
+            bulletSpawnController.SpawnRandomWaveFromPool();
+            lastSpawn = Time.time;
+        }
+    }
+}
